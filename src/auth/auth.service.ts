0330f0d3d06b5ea410c1145655231bdd6e94544d
Dmytro9 <dpoliuliuk@gmail.com>
2019-09-29 17:31:48 +0300

    auth without JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -10,7 +10,15 @@ export class AuthService {
     private userRepository: UserRepository,
     ) {}
 
-    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
-      return this.userRepository.signUp(authCredentialsDto);
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.signUp(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto) {
+    const username = await this.userRepository.validateUserPassword(authCredentialsDto);
+
+    if (!username) {
+      throw new UnauthorizedException('Invalid credentials');
     }
+  }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -25,11 +25,16 @@ export class UserRepository extends Repository<User> {
     }
   }
 
-  // async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
-  //   const { username, password } = authCredentialsDto;
-  //   const user = await this.findOne({ username });
-  //   return user;
-  // }
+  async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.findOne({ username });
+
+    if (user && await user.validatePassword(password)) {
+      return user.username;
+    } else {
+      return null;
+    }
+  }
 
   private async hashPassword(password: string, salt: string): Promise<string> {
     return bcrypt.hash(password, salt);
